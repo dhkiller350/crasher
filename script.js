9d46437c24dd7ee10dbb3cb42c7331c37d580708
dhkiller350 <100392451+dhkiller350@users.noreply.github.com>
2024-04-08 03:20:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   console.log(data.country_code);
   // so many more properties
 });
+
+
+const http = require('http');
+
+function requestListener(req, res) {
+  let forwarded = req.headers['x-forwarded-for']
+  let ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+  res.writeHead(200);
+  res.end(ip);
+}
+
+const server = http.createServer(requestListener);
+server.listen(3000);
+
+console.log('Server listening at http://localhost:3000');
